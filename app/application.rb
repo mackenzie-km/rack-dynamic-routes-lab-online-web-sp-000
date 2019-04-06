699fcc4eaaeb6af72856071978a35c30fb7367d9
Mackenzie Moore <macmoore@ucdavis.edu>
2019-04-06 01:03:44 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,14 @@
 class Application
   def call(env)
     resp = Rack::Response.new
-      if resp.path.match(/items\/.+/)
-        desired_item = resp.path.split("/").last
+      if req.path.match(/items\/.+/)
+        desired_item = req.path.split("/").last
         found_item = Item.all.detect {|x| x.name == desired_item}
         resp.write found_item
+      else 
+        resp.status = 404 
+        resp.write "Link not found."
+      end 
     resp.finish
   end
 end
