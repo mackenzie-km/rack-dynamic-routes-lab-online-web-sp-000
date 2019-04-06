b050d1c9685a2ebb4db1050209cdad2a7b7b9f81
Mackenzie Moore <macmoore@ucdavis.edu>
2019-04-06 01:07:04 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,20 @@
+class Application
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+      if req.path.match(/items\/.+/)
+        desired_item = req.path.split("/").last
+        found_item = Item.all.detect {|x| x.name == desired_item}
+        if found_item != nil
+          resp.write found_item.price
+        else
+          resp.status = 400
+          resp.write "Item not found"
+        end
+      else
+        resp.status = 404
+        resp.write "Route not found."
+      end
+    resp.finish
+  end
+end
