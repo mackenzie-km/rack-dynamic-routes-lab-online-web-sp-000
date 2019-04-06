e57aafe90c0981c42bf6364319b525bd12b2c2b4
Mackenzie Moore <macmoore@ucdavis.edu>
2019-04-06 01:03:50 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,10 +5,10 @@ class Application
         desired_item = req.path.split("/").last
         found_item = Item.all.detect {|x| x.name == desired_item}
         resp.write found_item
-      else 
-        resp.status = 404 
-        resp.write "Link not found."
-      end 
+      else
+        resp.status = 404
+        resp.write "Route not found."
+      end
     resp.finish
   end
 end
